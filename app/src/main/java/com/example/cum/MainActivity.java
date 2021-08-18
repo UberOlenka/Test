ffc037106eb8116bc944a6cb3a9bbf8ab7d7e97f
UberOlenka <lena.dovgo@gmail.com>
2021-08-18 11:58:37 +0300

    First update

---
+++ app/src/main/java/com/example/cum/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.cum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Random rnd = new Random();
+
+//      Находим View
+        Button buttonKnopka = findViewById(R.id.buttonKnopka);
+        TextView textVitalya = findViewById(R.id.textVitalya);
+
+//      Меняем текст при нажатии на кнопку
+        buttonKnopka.setOnClickListener(view -> {
+            int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+            int color2 = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+            textVitalya.setText("Говно собачье");
+            buttonKnopka.setText("Лобок");
+            buttonKnopka.setBackgroundColor(color);
+            textVitalya.setTextColor(color2);
+
+        });
+
+
+
+
+
+
+    }
+}
+
